1ced38bf7b107a880b5560ab81ec892c297aeb3c
quang <nguyenthequang080203@gmail.com>
2024-03-31 21:29:28 +0700

    initial commit

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: 'mongodb+srv://quang08:08022003@cluster0.rzapzwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+};
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dbConfig = require('./config/db');
+//const assetRoutes = require('./routes/assetRoutes');
+//const maintenanceRoutes = require('./routes/maintenanceRoutes');
+//const reportRoutes = require('./routes/reportRoutes');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('Connected to MongoDB'))
+ .catch(err => console.error('Error connecting to MongoDB:', err));
+
+// Routes
+//app.use('/assets', assetRoutes);
+//app.use('/maintenance', maintenanceRoutes);
+//app.use('/reports', reportRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
