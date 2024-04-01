e74cc48b8aca8d0190190651dc4634ae9fc8b59f
quang <nguyenthequang080203@gmail.com>
2024-04-01 16:09:50 +0700

    purchase basic setup

---
+++ controllers/purchaseController.js
@@ -0,0 +1,10 @@
+const Purchase = require("../models/Purchase");
+
+const purchaseController = {
+  createPurchase: async (req, res) => {
+    // Implement createPurchase controller function
+  },
+  // Implement other controller functions for CRUD operations
+};
+
+module.exports = purchaseController;
