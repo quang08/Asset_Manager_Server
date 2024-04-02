46565d906c812ff50eb8aec8188347c14c128dbb
hauozil543 <hauozil543@gmail.com>
2024-04-02 15:33:15 +0700

    add get maintenance/:id

--- routes/MaintenanceRoutes.js
+++ routes/MaintenanceRoutes.js
@@ -6,6 +6,7 @@ router.get("/", maintenanceController.getAllMaintenancerecords);
 router.post("/", maintenanceController.createMaintenance);
 router.put("/:id", maintenanceController.updateMaintenance);
 router.delete("/:id", maintenanceController.deleteMaintenance);
+router.get("/:id", maintenanceController.getMaintenanceById);
 
 
 //...
